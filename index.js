b89642a5ba3920b4002ad2a5136990843d0819ee
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-09-13 08:52:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  // options
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );
-    });
+io.on("connection", (socket) => {
+  // ...
 });
 
-http.listen(3000, () => console.log('listening on http://localhost:8080') );
-
-
-// Regular Websockets
-
-// const WebSocket = require('ws')
-// const server = new WebSocket.Server({ port: '8080' })
-
-// server.on('connection', socket => {
-
-//   socket.on('message', message => {
-
-//     socket.send(`Roger that! ${message}`);
-
-//   });
-
-// });
+httpServer.listen(3000);
