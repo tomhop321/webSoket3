3200fff347fd601b546c5cf37bac23d2020dab3e
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-09-13 09:47:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+const { Server } = require("socket.io");
+
+
+
+
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
